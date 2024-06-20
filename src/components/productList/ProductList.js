1bdc62aea7bcfba4c36b9b2641114d7093bb7ab3
Aravind A <aravind30052003@gmail.com>
2024-06-20 08:23:07 +0530

    Added my project to the repository

--- src/components/productList/ProductList.js
+++ src/components/productList/ProductList.js
@@ -3,6 +3,8 @@ import { ProductContext } from '../../context/ProductContext';
 import { Grid, Card, CardContent, CardMedia, Typography } from '@mui/material';
 import './ProductList.css';
 
+const placeholderImage = 'https://www.img2go.com/assets/img/backdoc.png';
+
 const ProductList = () => {
     const { products, error } = useContext(ProductContext);
 
@@ -11,16 +13,25 @@ const ProductList = () => {
     }
 
     return (
-        <Grid container spacing={2} className="product-list">
+        <Grid container spacing={4} className="product-list">
             {products.map(product => (
                 <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
                     <Card className="product-card">
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            image={product.image}
-                            alt={product.name}
-                        />
+                        {product.image ? (
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={product.image}
+                                alt={product.name}
+                            />
+                        ) : (
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={placeholderImage}
+                                alt="Placeholder"
+                            />
+                        )}
                         <CardContent>
                             <Typography gutterBottom variant="h5" component="div">
                                 {product.name}
